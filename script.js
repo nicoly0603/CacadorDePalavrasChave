fe346c2d7446e5b824c510ec008913bd2ffae8db
nicoly0603 <santos.nicoly06@escola.pr.gov.br>
2025-07-03 18:03:41 +0000

    arrumei de novo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-constant botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
