fce7bf0daad1e9716816cbf5b0f9f5057f172ed7
nicoly0603 <santos.nicoly06@escola.pr.gov.br>
2025-07-03 18:24:29 +0000

    processando texto

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 function mostraPalavraChave (){
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split("")
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavras.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto){
+    let palavras
 }
