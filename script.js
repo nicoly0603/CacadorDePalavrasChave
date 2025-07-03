09aa592fb71e9128637e7ed5604dce8029678b83
nicoly0603 <santos.nicoly06@escola.pr.gov.br>
2025-07-03 18:14:55 +0000

    selecionando apenas palavras chaves

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
 function mostraPalavraChave (){
     const texto = document.querySelector('#entrada-de-texto').value;
-    
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split("")
+
+    campoResultado.textContent = palavras;
 }
