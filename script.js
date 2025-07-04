facd73b26ef3824f68f38861f9c9aa9c71c3ffbd
nicoly0603 <santos.nicoly06@escola.pr.gov.br>
2025-07-04 18:30:35 +0000

    fim da aula 5

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
 
 
-    let frequencias = [];
+    let frequencias = {};
     for(let i of palavras){
         frequencias[i] = 0;
         for (let j of palavras){
-            if (palavras[i]==palavras[j]){
+            if (i == j){
                 frequencias[i]++;
             }
         }
