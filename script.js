87609b5b95e45c3fe73585ab76b74c37d885c620
nicoly0603 <santos.nicoly06@escola.pr.gov.br>
2025-07-04 18:27:45 +0000

    troquei o in pelo of

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function processaTexto(texto){
 
 
     let frequencias = [];
-    for(let i in palavras){
+    for(let i of palavras){
         frequencias[i] = 0;
-        for (let j in palavras){
+        for (let j of palavras){
             if (palavras[i]==palavras[j]){
                 frequencias[i]++;
             }
